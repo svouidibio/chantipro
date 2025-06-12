f191e2225b7bf02f8632c134ce3c01108dddaf61
svouidibio <77667196+svouidibio@users.noreply.github.com>
2025-06-12 04:14:25 +0200

    Fix import paths to use relative imports for components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import Header from '@/components/Header'
-import HeroSection from '@/components/HeroSection'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   const handleSubmit = (e: React.FormEvent) => {
